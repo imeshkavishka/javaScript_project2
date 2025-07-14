81f653edd93fad3783f025671591165782f07e94
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-14 10:41:32 +0530

    Calculator

---
+++ project2.js
@@ -0,0 +1,27 @@
+const num1 = prompt("Enter First Number : ");
+const num2 = prompt("Enter Second Number :");
+
+switch(operator){
+    case "+":
+        result = num1+num2;
+        console.log(result);
+        break;
+    case "-":
+        result = num1+num2;
+        console.log(result);
+        break;
+    case "/":
+        result = num1+num2;
+        console.log(result);
+        break;
+    case "*":
+        result = num1+num2;
+        console.log(result);
+        break;
+    case "+":
+        result = num1+num2;
+        console.log(result);
+        break;
+    default:
+        console.log("Invalid Character");
+}
