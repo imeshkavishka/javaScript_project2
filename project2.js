f824eb3d5bfc009b7d82b9f7b23833187a19ea21
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-14 11:25:03 +0530

    add loop to calculator

--- project2.js
+++ project2.js
@@ -1,28 +1,53 @@
 const prompt = require("prompt-sunc")()
-const num1 = parseFloat(prompt("Enter First Number : "))
-const num2 = parseFloat(prompt("Enter Second Number :"))
+
+let num1;
+let num2;
+while (true){
+    num1 = parseFloat(prompt("Enter First Number : "))
+    if (IsNan(num1)){
+        console.log("Invalid input")
+    }else{
+        break
+    }
+}
+
+while (true){
+    num2 = parseFloat(prompt("Enter Second Number :"))
+    if (IsNan(num2 )){
+        console.log("Invalid input")
+    }else{
+        break
+    }
+}
+
 const operator = prompt("Enter Sign")
 
 let result;
+let valid = true; 
 switch(operator){
     case "+":
         result = num1+num2
-        console.log(result)
+
         break;
     case "-":
         result = num1-num2
-        console.log(result)
+
         break;
     case "/":
         result = num1/num2
-        console.log(result)
+
         break;
     case "*":
         result = num1*num2
-        console.log(result)
+
         break;
 
     default:
         console.log("Invalid Character")
+        valid = false;
         break;
 }
+ 
+if(valid)
+    console.log(num1,operator,num2,"=",result)
+
